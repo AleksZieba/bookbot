db094b2b17e6c207dd1221747419ba2f32b7941d
AleksZieba <aleks@lepszyangielski.pl>
2024-12-13 13:12:22 +0100

    Adding final print functionality, in progress.

--- main.py
+++ main.py
@@ -3,7 +3,13 @@ def main():
     text = get_book_text(book_path) 
     word_count = get_word_count(text) 
     char_count = get_character_count(text)
-    print(f"=== Report on {book_path} ===")
+    char_list = get_character_report(char_count) 
+    print(char_list)
+    #print(f"""=== Report on {book_path} ===
+   # {word_count} words found in this book. 
+   # 
+   # {char_list}
+    #=== End of Report ===""")
 
 def get_book_text(path): 
     with open(path) as f:
@@ -22,8 +28,15 @@ def get_character_count(text):
         character_count[character] = character_count.get(character, 0) + 1
     return character_count 
 
-#def get_character_report(char_count): 
-    #char_count_list = []
-    
+def get_character_report(char_count): 
+    char_count_list = [] 
+    for character in char_count:
+        if character.isalpha() == True:
+            char = {"character": character, "count": char_count[character]}
+            char_count_list.append(char)
+    return char_count_list 
 
-main() 
+def sort_by(dict):
+    return dict["num"]
+
+main()
